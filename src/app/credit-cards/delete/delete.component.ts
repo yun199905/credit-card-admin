ece1aa3bbfae9cb7a2c371774e31a62252436afe
cindy <Cindy.He@hepiuscare.com.tw>
2024-10-01 17:17:11 +0800

    feat: 🎸 add delete functionality for credit card

--- src/app/credit-cards/delete/delete.component.ts
+++ src/app/credit-cards/delete/delete.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, numberAttribute } from '@angular/core';
+import { CreditCard } from '../../models/credit-cards';
+import { CreditCardsService } from '../../services/credit-cards.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete',
@@ -8,5 +11,26 @@ import { Component } from '@angular/core';
   styleUrl: './delete.component.scss'
 })
 export class DeleteComponent {
+  #creditCardsService = inject(CreditCardsService);
+  #router = inject(Router);
+  creditCardId!: number;
 
+  @Input({ transform: numberAttribute }) id!: number;
+
+  ngOnInit() {
+    this.creditCardId = this.id;
+
+    if (this.creditCardId) {
+      this.#creditCardsService.deleteCreditCard(this.creditCardId)
+      .subscribe({
+        next: () => {
+          alert("Credit card deleted successfully");
+          this.#router.navigate(['/credit-cards']);
+        },
+        error: (error) => {
+          console.error('Error deleting credit card:', error);
+        }
+      });
+    }
+  }
 }
